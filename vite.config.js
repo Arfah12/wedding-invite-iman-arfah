1613e0be23971e66d3ed926531e3284d9ad3232e
Arfah Danisya <arfahdanisya@gmail.com>
2025-12-02 17:35:37 +0800

    Fix: Vite base path for Netlify white screen issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: true, // supaya boleh diakses luar localhost
     port: 5173, // optional, ikut port dev server awak
